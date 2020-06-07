eed18bb9bdaa4fab6a51eca066109ee1aac89903
esinghrahul <e.singh.rahul@outlook.com>
2020-06-08 00:26:29 +0530

    Updates

---
+++ precache-manifest.9fcb4773aae97440cc61d05833d97a28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "608d8ae3c3c00d6dbc350ad95aa5d6c5",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "baac2bd7eef1067e692b",
+    "url": "/monsters-rolodex/static/css/main.f9036c1e.chunk.css"
+  },
+  {
+    "revision": "12240f73676d8f2fbd85",
+    "url": "/monsters-rolodex/static/js/2.a3c82928.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a3c82928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "baac2bd7eef1067e692b",
+    "url": "/monsters-rolodex/static/js/main.9fada186.chunk.js"
+  },
+  {
+    "revision": "6062e4a584c29c6e0372",
+    "url": "/monsters-rolodex/static/js/runtime-main.e3d252a9.js"
+  }
+]);
